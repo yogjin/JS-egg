87e9abd96c785d1aa58438eee8be375ab7c666fe
yogjin <youngjin988@gmail.com>
2022-02-04 23:36:27 +0900

    feat: Add vertical,horizen line follwing mousemove

--- Web-APIs/coordinates-shot-007/main.js
+++ Web-APIs/coordinates-shot-007/main.js
@@ -1,8 +1,15 @@
 const target = document.querySelector('.target');
+const col = document.querySelector('.col');
+const row = document.querySelector('.row');
+
 document.addEventListener('mousemove', (e) => {
   const offsetX = e.clientX;
   const offsetY = e.clientY;
   // 조준점
   target.style.left = `${offsetX - 60}px`;
   target.style.top = `${offsetY - 60}px`;
+
+  // 선
+  col.style.left = `${offsetX}px`;
+  row.style.top = `${offsetY}px`;
 });
