501333f06caa16f64791fed76d95ec7cabe0107e
yogjin <youngjin988@gmail.com>
2022-02-04 23:50:12 +0900

    feat: Add target coordinates

--- Web-APIs/coordinates-shot-007/main.js
+++ Web-APIs/coordinates-shot-007/main.js
@@ -1,6 +1,7 @@
 const target = document.querySelector('.target');
 const col = document.querySelector('.col');
 const row = document.querySelector('.row');
+const coordinates = document.querySelector('.coordinates');
 
 document.addEventListener('mousemove', (e) => {
   const offsetX = e.clientX;
@@ -12,4 +13,9 @@ document.addEventListener('mousemove', (e) => {
   // 선
   col.style.left = `${offsetX}px`;
   row.style.top = `${offsetY}px`;
+
+  // 좌표
+  coordinates.innerHTML = `${offsetX}px, ${offsetY}px`;
+  coordinates.style.left = `${offsetX + 30}px`;
+  coordinates.style.top = `${offsetY + 30}px`;
 });
