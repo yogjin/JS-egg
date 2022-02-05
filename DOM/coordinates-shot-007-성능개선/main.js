6fb798c3f88a18f264e6b0572f4a83991c62cff7
yogjin <youngjin988@gmail.com>
2022-02-06 02:51:09 +0900

    refactor: change const

--- DOM/coordinates-shot-007-성능개선/main.js
+++ DOM/coordinates-shot-007-성능개선/main.js
@@ -4,18 +4,18 @@ const row = document.querySelector('.row');
 const coordinates = document.querySelector('.coordinates');
 
 document.addEventListener('mousemove', (e) => {
-  const clientX = e.clientX;
-  const clientY = e.clientY;
+  const x = e.clientX;
+  const y = e.clientY;
   // 조준점
-  target.style.left = `${clientX - 60}px`;
-  target.style.top = `${clientY - 60}px`;
+  target.style.left = `${x - 60}px`;
+  target.style.top = `${y - 60}px`;
 
   // 선
-  col.style.left = `${clientX}px`;
-  row.style.top = `${clientY}px`;
+  col.style.left = `${x}px`;
+  row.style.top = `${y}px`;
 
   // 좌표
-  coordinates.innerHTML = `${clientX}px, ${clientY}px`;
-  coordinates.style.left = `${clientX + 30}px`;
-  coordinates.style.top = `${clientY + 30}px`;
+  coordinates.innerHTML = `${x}px, ${y}px`;
+  coordinates.style.left = `${x + 30}px`;
+  coordinates.style.top = `${y + 30}px`;
 });
