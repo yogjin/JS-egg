02f65f9fe16b5ef2831266eea4fcc694df36530a
yogjin <youngjin988@gmail.com>
2022-02-10 22:55:33 +0900

    feat: 벌레, 당근을 play_ground의 랜덤위치에 위치시키는 기능 추가

--- Projects/carrot/main.js
+++ Projects/carrot/main.js
@@ -1,5 +1,11 @@
 const playButton = document.querySelector('.play_button');
 const countDown = document.querySelector('.count_down');
+const playGround = document.querySelector('.play_ground');
+const groundWidth = playGround.getBoundingClientRect().width;
+const groundHeight = playGround.getBoundingClientRect().height;
+const groundX = playGround.getBoundingClientRect().x;
+const groundY = playGround.getBoundingClientRect().y;
+
 let countInterval;
 
 // 게임 시작하면 카운트 다운
@@ -30,3 +36,38 @@ function handleCountDown() {
   }
 }
 playButton.addEventListener('click', handleCountDown);
+
+// 벌레, 당근 추가
+function setCarrotAndBugs() {
+  const carrot = `
+    <div class='item carrot'>
+      <img src='./img/carrot.png'>
+    </div>
+  `;
+  const bug = `
+    <div class='item bug'>
+      <img src='./img/bug.png'>
+    </div>
+  `;
+  playGround.insertAdjacentHTML('afterbegin', carrot.repeat(10));
+  playGround.insertAdjacentHTML('afterbegin', bug.repeat(10));
+}
+// 벌레, 당근을 랜덤위치 시킴
+// img가 load되고 나서 getRandomPosition안의 getBoundingClientRect() 실행가능함
+window.addEventListener('load', () => {
+  const carrotAndBugs = document.querySelectorAll('.item');
+  carrotAndBugs.forEach((carrotOrBug) => {
+    const { x, y } = getRandomPosition(carrotOrBug);
+    carrotOrBug.style.left = `${x}px`;
+    carrotOrBug.style.top = `${y}px`;
+  });
+});
+
+// 벌레,당근을 playGround안 랜덤한 위치에 위치시키기 위함
+function getRandomPosition(carrotOrBug) {
+  const carrotOrBugWidth = carrotOrBug.getBoundingClientRect().width;
+  const carrotOrBugHeight = carrotOrBug.getBoundingClientRect().height;
+  const x = groundX + (groundWidth - carrotOrBugWidth) * Math.random();
+  const y = groundY + (groundHeight - carrotOrBugHeight) * Math.random();
+  return { x, y };
+}
