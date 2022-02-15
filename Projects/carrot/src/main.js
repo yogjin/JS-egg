3c48bfdecccca75540c768d7d694e15e05a97549
yogjin <youngjin988@gmail.com>
2022-02-15 14:25:53 +0900

    refactor: Add playGround.js: 게임공간 리팩토링

--- Projects/carrot/src/main.js
+++ Projects/carrot/src/main.js
@@ -1,14 +1,8 @@
 import Result from './result.js';
+import PlayGround from './playGround.js';
 const playButton = document.querySelector('.play_button');
 const countDown = document.querySelector('.count_down');
-const playGround = document.querySelector('.play_ground');
 const remainingCarrot = document.querySelector('.remaining_carrot');
-let groundWidth;
-let groundHeight;
-window.addEventListener('load', () => {
-  groundWidth = playGround.getBoundingClientRect().width;
-  groundHeight = playGround.getBoundingClientRect().height;
-});
 
 let countInterval;
 let numOfCarrots;
@@ -18,14 +12,14 @@ const carrotSound = new Audio('./sound/carrot_pull.mp3');
 const bugSound = new Audio('./sound/bug_pull.mp3');
 const winSound = new Audio('./sound/game_win.mp3');
 const alertSound = new Audio('./sound/alert.wav');
-const carrotSize = 80;
 
 const gameFinishBanner = new Result();
 gameFinishBanner.setClickListener(gameStart);
+const playGround = new PlayGround();
+playGround.setClickListener(handleClicked);
 
 // EventListener
 playButton.addEventListener('click', gameStart);
-playGround.addEventListener('click', handleClicked);
 
 // 카운트 다운 시간 설정, 시작
 function startCountDown(sec) {
@@ -59,46 +53,6 @@ function handleCountDown() {
     playButton.innerHTML = `<i class="fas fa-play"></i>`;
     stopSound(bgSound);
   }
-}
-
-// 벌레, 당근 추가
-function addCarrotAndBugs() {
-  const carrot = `
-    <div class='item carrot'>
-      <img src='./img/carrot.png' data-id='carrot'>
-    </div>
-  `;
-  const bug = `
-    <div class='item bug'>
-      <img src='./img/bug.png' data-id='bug'>
-    </div>
-  `;
-  playGround.innerHTML = `${carrot.repeat(10)}${bug.repeat(10)}`;
-}
-// 벌레, 당근을 랜덤위치 시킴
-// img가 load되고 나서 getRandomPosition안의 getBoundingClientRect() 실행가능함
-/* 수정요망 */
-/* playButton을 눌렀을때, getBoundingClientRect() 제대로 작동하게 해야함. load말고 Promise같은걸로, 또는 다른 방법으로 */
-function setCarrotAndBugs() {
-  if (!isPlaying) {
-    return;
-  }
-  const carrotAndBugs = document.querySelectorAll('.item');
-  carrotAndBugs.forEach((carrotOrBug) => {
-    carrotOrBug.style.display = 'inline-block';
-    const { x, y } = getRandomPosition();
-    carrotOrBug.style.left = `${x}px`;
-    carrotOrBug.style.top = `${y}px`;
-  });
-}
-
-// 벌레,당근을 playGround안 랜덤한 위치에 위치시키기 위함
-function getRandomPosition() {
-  const rangeX = groundWidth - carrotSize;
-  const rangeY = groundHeight - carrotSize;
-  const x = rangeX * Math.random();
-  const y = rangeY * Math.random();
-  return { x, y };
 }
 
 // play_ground를 클릭했을 때 상호작용
@@ -135,8 +89,8 @@ function gameStart() {
   playButton.style.visibility = 'visible';
   handleCountDown();
   setRemainingCarrot(10);
-  addCarrotAndBugs();
-  setCarrotAndBugs();
+  playGround.addCarrotAndBugs();
+  playGround.setCarrotAndBugs();
 }
 
 // 게임 오버
--- Projects/carrot/src/playGround.js
+++ Projects/carrot/src/playGround.js
@@ -1,3 +1,53 @@
-class PlayGround {
-  constructor() {}
+export default class PlayGround {
+  constructor() {
+    this.playGround = document.querySelector('.play_ground');
+    this.carrotSize = 80;
+    window.addEventListener('load', () => {
+      this.groundWidth = this.playGround.getBoundingClientRect().width;
+      this.groundHeight = this.playGround.getBoundingClientRect().height;
+    });
+    this.playGround.addEventListener('click', (e) => {
+      this.onClick && this.onClick(e);
+    });
+  }
+  // 벌레, 당근 클릭시 이벤트 설정
+  setClickListener(onClick) {
+    this.onClick = onClick;
+  }
+  // 벌레, 당근 추가
+  addCarrotAndBugs() {
+    const carrot = `
+    <div class='item carrot'>
+      <img src='./img/carrot.png' data-id='carrot'>
+    </div>
+  `;
+    const bug = `
+    <div class='item bug'>
+      <img src='./img/bug.png' data-id='bug'>
+    </div>
+  `;
+    this.playGround.innerHTML = `${carrot.repeat(10)}${bug.repeat(10)}`;
+  }
+
+  // 벌레,당근을 playGround안 랜덤한 위치에 위치시키기 위함
+  getRandomPosition() {
+    const rangeX = this.groundWidth - this.carrotSize;
+    const rangeY = this.groundHeight - this.carrotSize;
+    const x = rangeX * Math.random();
+    const y = rangeY * Math.random();
+    return { x, y };
+  }
+  // 벌레, 당근을 랜덤위치 시킴
+  setCarrotAndBugs() {
+    if (!isPlaying) {
+      return;
+    }
+    const carrotAndBugs = document.querySelectorAll('.item');
+    carrotAndBugs.forEach((carrotOrBug) => {
+      carrotOrBug.style.display = 'inline-block';
+      const { x, y } = this.getRandomPosition();
+      carrotOrBug.style.left = `${x}px`;
+      carrotOrBug.style.top = `${y}px`;
+    });
+  }
 }
